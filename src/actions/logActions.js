00606106c93a8c30144edc9a4f03dcf60c137d04
David Stinnett <davidstinnett@icloud.com>
2021-02-01 22:43:26 -0500

    can now add logs

---
+++ src/actions/logActions.js
@@ -0,0 +1,68 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
+
+// export const getLogs = () => {
+// 	return async (dispatch) => {
+// 		setLoading();
+
+// 		const res = await fetch("/logs");
+// 		const data = await res.json();
+
+// 		dispatch({
+// 			type: GET_LOGS,
+// 			payload: data,
+// 		});
+// 	};
+// };
+
+//GET LOGS FROM SERVER
+export const getLogs = () => async (dispatch) => {
+	try {
+		setLoading();
+
+		const res = await fetch("/logs");
+		const data = await res.json();
+
+		dispatch({
+			type: GET_LOGS,
+			payload: data,
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data,
+		});
+	}
+};
+
+//ADD NEW LOG
+export const addLog = (log) => async (dispatch) => {
+	try {
+		setLoading();
+
+		const res = await fetch("/logs", {
+			method: "POST",
+			body: JSON.stringify(log),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		const data = await res.json();
+
+		dispatch({
+			type: ADD_LOG,
+			payload: data,
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data,
+		});
+	}
+};
+
+//SET LOADING TO TRUE
+export const setLoading = () => {
+	return {
+		type: SET_LOADING,
+	};
+};
